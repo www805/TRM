9181f77091a572fc749a021f3a91278d0c500895
wub <874172523@qq.com>
2019-04-09 16:31:14 +0800

    添加定时器模块

--- SourseCode/TRM/src/main/java/com/avst/trm/TrmApplication.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/TrmApplication.java
@@ -3,9 +3,11 @@ package com.avst.trm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan({"com.avst.trm.v1.common.datasourse.base.mapper","com.avst.trm.v1.common.datasourse.police.mapper"})
+@EnableScheduling
 public class TrmApplication {
 
     public static void main(String[] args) {
