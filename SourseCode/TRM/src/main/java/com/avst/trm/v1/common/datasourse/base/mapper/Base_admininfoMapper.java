ea3d351aa7402a5bf8087191abc94cd6547f7a5b
wub <874172523@qq.com>
2019-04-10 15:38:21 +0800

    param添加

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/common/datasourse/base/mapper/Base_admininfoMapper.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/common/datasourse/base/mapper/Base_admininfoMapper.java
@@ -18,7 +18,11 @@ import java.util.List;
  * @since 2019-04-09
  */
 public interface Base_admininfoMapper extends BaseMapper<Base_admininfo> {
-    @Select("select a.* ,ar.id admintoroleid,ar.adminid,ar.roleid from base_admininfo a left join base_admintorole ar on a.id=ar.adminid where a.id != ${adminid}")
+    @Select("select a.* ,ar.id admintoroleid,ar.adminid,ar.roleid " +
+            "from base_admininfo a " +
+            "left join base_admintorole ar " +
+            "on a.id=ar.adminid " +
+            "where a.id != ${adminid}")
     public List<AdminAndAdmintorole> getAdminAndAdmintorolelist(Page page, GetAdminAndAdmintorolelistParam param);
 
     @Select("select count(a.id) from base_admininfo a " +
