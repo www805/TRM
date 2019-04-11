5e54c6a06b061275d89a810a53c2e6a738b2e142
zjl <zjl@qq.com>
2019-04-11 18:49:40 +0800

    4-11下班前提交

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/web/action/CeshiAction.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/web/action/CeshiAction.java
@@ -1,5 +1,7 @@
 package com.avst.trm.v1.web.action;
 
+import com.avst.trm.v1.common.datasourse.base.entity.Base_role;
+import com.avst.trm.v1.common.datasourse.base.mapper.Base_roleMapper;
 import com.avst.trm.v1.common.util.baseaction.BaseAction;
 import com.avst.trm.v1.common.util.baseaction.RResult;
 import com.avst.trm.v1.web.service.CeshiService;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/web/ceshi")
@@ -47,7 +51,7 @@ public class CeshiAction extends BaseAction{
     public ModelAndView getuserbyid(Model model,@PathVariable("id")String id) {
 
         model.addAttribute("title", "第"+id+"个人");
-        return new ModelAndView("users/view", "userModel", model);
+        return new ModelAndView("role", "userModel", model);
 
     }
 
@@ -57,6 +61,77 @@ public class CeshiAction extends BaseAction{
         model.addAttribute("title", "layui测试主页");
 
         return new ModelAndView("main", "main", model);
+
+    }
+
+    /***
+     * 用户列表
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/getUserList")
+    public ModelAndView getUserList(Model model) {
+
+        RResult rResult=createNewResultOfFail();
+        ceshiService.getadminlist3(rResult);
+
+        model.addAttribute("RResult", rResult);
+        model.addAttribute("title", "用户列表");
+        return new ModelAndView("users/list", "userModel", model);
+
+    }
+
+    @Autowired
+    private Base_roleMapper base_roleMapper;
+
+    @GetMapping(value = "/getUserRole")
+    public ModelAndView getUserRole(Model model) {
+
+        RResult rResult=createNewResultOfFail();
+        //ceshiService.getadminlist3(rResult);
+
+        List<Base_role> base_roles = base_roleMapper.selectList(null);
+
+        rResult.setData(base_roles);
+
+        model.addAttribute("RResult", rResult);
+        model.addAttribute("title", "角色列表");
+        return new ModelAndView("users/role", "roleModel", model);
+
+    }
+
+    /**
+     * 首页
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/home")
+    public ModelAndView home(Model model) {
+
+        RResult rResult=createNewResultOfFail();
+        //ceshiService.getadminlist3(rResult);
+
+        model.addAttribute("RResult", rResult);
+        model.addAttribute("title", "首页");
+        return new ModelAndView("home", "homeModel", model);
+
+    }
+
+
+    /**
+     * 系统配置
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/serverconfig")
+    public ModelAndView serverconfig(Model model) {
+
+        RResult rResult=createNewResultOfFail();
+        //ceshiService.getadminlist3(rResult);
+
+        model.addAttribute("RResult", rResult);
+        model.addAttribute("title", "系统配置");
+        return new ModelAndView("serverconfig", "configModel", model);
 
     }
 
