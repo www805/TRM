71a8ecb0f693295c119b89e6d3b6c00de7279782
wub <874172523@qq.com>
2019-04-12 17:49:40 +0800

    模板引擎的分页处理

---
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/web/req/Getlist3Param.java
@@ -0,0 +1,16 @@
+package com.avst.trm.v1.web.param;
+
+import com.avst.trm.v1.common.util.baseaction.Page;
+
+public class Getlist3Param extends Page {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/web/vo/Getlist3VO.java
@@ -0,0 +1,4 @@
+package com.avst.trm.v1.web.vo;
+
+public class Getlist3Param {
+}
