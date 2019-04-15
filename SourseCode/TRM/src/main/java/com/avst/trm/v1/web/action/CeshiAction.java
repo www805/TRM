79215a3a824dda5939d947205526593a70ee49b2
wub <874172523@qq.com>
2019-04-15 09:35:04 +0800

    模板引擎的分页处理

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/web/action/CeshiAction.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/web/action/CeshiAction.java
@@ -4,47 +4,47 @@ import com.avst.trm.v1.common.util.baseaction.BaseAction;
 import com.avst.trm.v1.common.util.baseaction.RResult;
 import com.avst.trm.v1.web.service.CeshiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/web/ceshi")
 public class CeshiAction extends BaseAction{
 
     @Autowired
     private CeshiService ceshiService;
 
-    @GetMapping(value = "/ceshi")
+    @RequestMapping(value = "/ceshi")
     public RResult getlist(String username) {
         RResult rResult=createNewResultOfFail();
         ceshiService.getadminlist(rResult,username);
         return rResult;
     }
 
-    @GetMapping(value = "/ceshi2")
+    @RequestMapping(value = "/ceshi2")
     public RResult getlist2(int size) {
         RResult rResult=createNewResultOfFail();
         ceshiService.getadminlist2(rResult,size);
         return rResult;
     }
 
-    @GetMapping(value = "/ceshi3")
+    @RequestMapping(value = "/ceshi3")
     public ModelAndView getlist3(Model model) {
         RResult rResult=createNewResultOfFail();
         ceshiService.getadminlist3(rResult);
 
         model.addAttribute("RResult", rResult);
         model.addAttribute("title", "用户管理");
-        model.addAttribute("title2", "用户管理");
+
         return new ModelAndView("users/list", "userModel", model);
 
     }
 
-    @GetMapping(value = "/users/{id}")
+    @RequestMapping(value = "/users/{id}")
     public ModelAndView getuserbyid(Model model,@PathVariable("id")String id) {
 
         model.addAttribute("title", "第"+id+"个人");
@@ -52,7 +52,7 @@ public class CeshiAction extends BaseAction{
 
     }
 
-    @GetMapping(value = "/gotomain")
+    @RequestMapping(value = "/gotomain")
     public ModelAndView gotomain(Model model) {
 
         model.addAttribute("title", "layui测试主页");
@@ -62,7 +62,7 @@ public class CeshiAction extends BaseAction{
     }
 
 
-    @GetMapping(value = "/gotologin")
+    @RequestMapping(value = "/gotologin")
     public ModelAndView gotologin(Model model) {
 
         model.addAttribute("title", "layui测试主页");
@@ -71,8 +71,5 @@ public class CeshiAction extends BaseAction{
 
     }
 
-    /**
-     * 测试提交ppdsafhjdsyhfjhhjhdjfgsdf
-     */
 
 }
