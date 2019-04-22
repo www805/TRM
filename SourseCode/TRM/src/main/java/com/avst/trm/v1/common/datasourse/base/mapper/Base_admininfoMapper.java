e0abdb894e310959df6cc07fde4305b2faa5c881
wub <874172523@qq.com>
2019-04-22 11:55:58 +0800

    关于web后台的框架规划调整

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/common/datasourse/base/mapper/Base_admininfoMapper.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/common/datasourse/base/mapper/Base_admininfoMapper.java
@@ -22,7 +22,7 @@ import java.util.List;
  * @since 2019-04-09
  */
 public interface Base_admininfoMapper extends BaseMapper<Base_admininfo> {
-    @Select("select a.* ,ar.id admintoroleid,ar.adminid,ar.roleid " +
+    @Select("select a.* ,ar.id admintoroleid,ar.adminssid,ar.rolessid " +
             "from base_admininfo a left join base_admintorole ar on a.ssid=ar.adminssid ")
     public List<AdminAndAdmintorole> getAdminAndAdmintorolelist(Page page, EntityWrapper ew);
 
