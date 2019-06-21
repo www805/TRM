80770b1704080c475ea5c3072b05597ec5301340
zjl <zjl@qq.com>
2019-06-21 15:49:06 +0800

    修改了提供给总控的返回参数

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/outsideinterface/offerclientinterface/v1/police/service/ControlInfoService.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/outsideinterface/offerclientinterface/v1/police/service/ControlInfoService.java
@@ -1,7 +1,5 @@
 package com.avst.trm.v1.outsideinterface.offerclientinterface.v1.police.service;
 
-import com.avst.trm.v1.common.util.StatusCode;
-import com.avst.trm.v1.outsideinterface.offerclientinterface.v1.police.vo.ControlInfoParamVO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +14,13 @@ public class ControlInfoService {
      * 获取本服务器信息
      * @return
      */
-    public ControlInfoParamVO getControlInfo(){
-        //返回本服务的信息
-        ControlInfoParamVO infoVO = new ControlInfoParamVO();
-
-        infoVO.setCount(1);//数量
-        infoVO.setStatus(StatusCode.OK);//成功状态
-
-        return infoVO;
-    }
+//    public ControlInfoParamVO getControlInfo(){
+//        //返回本服务的信息
+//        ControlInfoParamVO infoVO = new ControlInfoParamVO();
+//
+//        infoVO.setCount(1);//数量
+//        infoVO.setStatus(StatusCode.OK);//成功状态
+//
+//        return infoVO;
+//    }
 }
