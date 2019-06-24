f9de7d14d57fcfa429be576ca69903e021122e7c
zjl <zjl@qq.com>
2019-06-24 15:36:26 +0800

    请求总控接口跟当前服务器对比修改

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/feignclient/zk/ZkControl.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/feignclient/zk/ZkControl.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @FeignClient(value="zk",url="localhost:8079/")
 public interface ZkControl {
 
+    //获取注册的服务器
     @RequestMapping( value = "/zk/getControlInfoAll")
     @ResponseBody
     public RResult getControlInfoAll();
 
+    //获取服务器时间
+    @RequestMapping( value = "/zk/getControlTime")
+    @ResponseBody
+    public RResult getControlTime();
 }
