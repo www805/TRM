5dd26a1cf5c7b377b64b6f72bb9a82947cc96dd7
wb <874172523@qq.com>
2019-08-02 11:24:16 +0800

    定时器调整

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/common/conf/Scheduler.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/common/conf/Scheduler.java
@@ -55,7 +55,7 @@ public class Scheduler {
     /**
      * 需要验证
      */
-    @Scheduled(cron = "0 1/1 * * * *")
+    @Scheduled(cron = "0 1/5 * * * ?")
     public void testTasks2() {
 
         LogUtil.intoLog(this.getClass(),"定时任务执行时间testTasks2：" + dateFormat.format(new Date()));
