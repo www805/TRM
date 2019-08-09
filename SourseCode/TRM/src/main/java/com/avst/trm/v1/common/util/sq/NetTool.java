ba68989cd3dbb94fda7d55af2780d7d63b2e4c02
zjl <zjl@qq.com>
2019-08-09 14:58:14 +0800

    修改了exec命令，@Value等等

--- SourseCode/TRM/src/main/java/com/avst/trm/v1/common/util/sq/NetTool.java
+++ SourseCode/TRM/src/main/java/com/avst/trm/v1/common/util/sq/NetTool.java
@@ -396,4 +396,61 @@ public static String getLocalMac() {
     	}).start();
 	}
 
+	/**
+	 * 执行cmd命令
+	 * @param cmd
+	 * @return
+	 */
+	public static String executeCMD(String cmd){
+		OutputStream os = null;
+		InputStream in=null;
+		InputStream in2=null;
+		Process process=null;
+		try {
+			long start = System.currentTimeMillis();
+			process = Runtime.getRuntime().exec(cmd);
+			os=process.getOutputStream();
+			in=process.getInputStream();
+			in2=process.getErrorStream();
+
+			printMessage(in2);
+
+			int exitvalue=process.waitFor();
+			if(exitvalue!=0){
+				throw new Exception("exitvalue is not 0, 说明代码有错");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+
+			try {
+				if(null!=os){
+					os.flush();
+					os.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if(null!=process){
+					process.destroy();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if(null!=in){
+					in.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return null;
+
+	}
 }  
